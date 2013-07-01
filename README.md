ffb31c6998cb929990a91429faee297fed0328d0
Vinci <vinci.rufus@gmail.com>
2013-07-01 19:01:53 +0530

    Mind Map of the features still WIP

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-the-dignity-project
-===================
+# THe Dignity Project
 
-The techside evolution of the Dignity Project
+
+This is evolution of the Dignity Project on the Web. It will contain the mindmaps, UI design and the Code base of The Dignity Project website. Please feel free to contribute to this in whichever way you think you can.
---
+++ mind-map/the-dignity-project-mindmap.mm
@@ -0,0 +1,26 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1372672177484" ID="ID_1172359469" MODIFIED="1372680132111" STYLE="bubble" TEXT="The Dignity Project" VGAP="0">
+<node CREATED="1372672939687" HGAP="36" ID="ID_735489900" MODIFIED="1372684677071" POSITION="right" STYLE="bubble" TEXT="Listing Page">
+<node CREATED="1372680147507" ID="ID_1632306851" MODIFIED="1372680926344" TEXT="The Details Page">
+<node CREATED="1372680204936" ID="ID_1689906315" MODIFIED="1372680239012" TEXT="It shows a timeline of the various milestones of that particular toilet"/>
+<node CREATED="1372680239856" ID="ID_789139919" MODIFIED="1372680253901" TEXT="Shows the sponsors of this Toilet"/>
+<node CREATED="1372680254423" ID="ID_685415032" MODIFIED="1372680276098" TEXT="If the sponsor is there available then show the CTA for Sponsorship"/>
+<node CREATED="1372680276685" ID="ID_384334131" MODIFIED="1372680306483" TEXT="Show the families who will benefit from this toilet. ( photos and writeup)"/>
+<node CREATED="1372672997812" HGAP="10" ID="ID_498359410" MODIFIED="1372680989876" STYLE="bubble" TEXT="Show Status [ planned / In progress / Completed]" VSHIFT="-64"/>
+<node CREATED="1372681005840" ID="ID_1037498432" MODIFIED="1372681040412" TEXT="How to get Involved">
+<node CREATED="1372673392093" HGAP="35" ID="ID_1518666823" MODIFIED="1372673669437" TEXT="Tell friends/ Recommend" VSHIFT="-31"/>
+<node CREATED="1372673402093" HGAP="45" ID="ID_333073138" MODIFIED="1372674893703" TEXT="Donate" VSHIFT="19"/>
+</node>
+</node>
+<node CREATED="1372684679362" ID="ID_985159315" MODIFIED="1372684724559" TEXT="Display a Google Map with locations of all toilets {planned / in progress / Completed]"/>
+<node CREATED="1372684739643" ID="ID_1962575560" MODIFIED="1372684753361" TEXT="Color of pointer will indicate status of the toilet"/>
+</node>
+<node CREATED="1372684763112" ID="ID_1513427924" MODIFIED="1372684769793" POSITION="right" TEXT="Progress Report Summary Page"/>
+<node COLOR="#000000" CREATED="1372673792500" ID="ID_673903731" MODIFIED="1372685460631" POSITION="left" STYLE="bubble" TEXT="About us ">
+<node CREATED="1372673183718" HGAP="16" ID="ID_1506816436" MODIFIED="1372685460631" STYLE="bubble" TEXT="Real Story" VSHIFT="85"/>
+<node CREATED="1372676212453" HGAP="50" ID="ID_1273307480" MODIFIED="1372685460631" TEXT="Presence/Locations" VSHIFT="114"/>
+<node CREATED="1372675120703" HGAP="-49" ID="ID_601566347" MODIFIED="1372685460631" TEXT="About Sanitation" VSHIFT="-64"/>
+</node>
+</node>
+</map>
